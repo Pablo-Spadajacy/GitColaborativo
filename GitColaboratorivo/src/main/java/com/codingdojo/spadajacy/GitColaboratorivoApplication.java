d6c0e05893158ef8a63032a9db882eaa87ff85c5
Pablo Quiroga <HockatYT@gmail.com>
2024-07-03 20:18:34 -0300

    inicialización

---
+++ GitColaboratorivo/src/main/java/com/codingdojo/spadajacy/GitColaboratorivoApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.spadajacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitColaboratorivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitColaboratorivoApplication.class, args);
+	}
+
+}
